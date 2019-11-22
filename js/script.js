cac78b3a15a6e369a4d026c88e4cdabd69b97231
Mubaraq Wahab <mub.wahab26@gmail.com>
2019-11-22 19:58:42 +0100

    Use template on top pages

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ $(window).scroll(function() {
   let scrollThreshold = $('.jumbotron').outerHeight();
   $('.navbar').toggleClass('scrolled', $(this).scrollTop() >= scrollThreshold);
 
-  if($(this).scrollTop() >= scrollThreshold - 200) {
+  if($(this).scrollTop() >= (2*scrollThreshold/5)) {
     $('.header-angle-down').hide();
   }
 });
 
 $('.navbar-toggler').on('click', function() {
-  $('.navbar-toggler i').toggleClass('fa-minus').toggleClass('text-light');
+  $('.navbar-toggler i').toggleClass('fa-bars').toggleClass('fa-minus').toggleClass('text-light');
 });
 
 $('.header-angle-down').on('click', function() {
